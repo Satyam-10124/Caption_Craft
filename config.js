bc11d1a98efc30d0dc6df6a18d6f61fa5ea67bc1
Arpit-Singh320 <arpit2005singh@gmail.com>
2025-01-31 01:23:21 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    HUGGING_FACE_TOKEN: "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",
+    GROQ_API_KEY: "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"
+};
+
+export default config;
